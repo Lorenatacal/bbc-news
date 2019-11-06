389b11647d8ad5408a7f1bff4737c9605c8fb203
Lorena <lorena.tacal@gmail.com>
2019-11-06 15:23:41 +0000

    Made Api Call and fetched the data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
+// Your API key is: 51dde7a7caac40d5af679aa8eedde373
 function App() {
+  const [data, setData] = React.useState();
+  const [typedInput, setTypedInput] = React.useState("")
+
+  console.log(typedInput)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onChange={event => setTypedInput(event.target.value)} />
+      <button onClick={() => {
+        axios
+          .get(`https://newsapi.org/v2/everything?domains=wsj.com,nytimes.com&apiKey=51dde7a7caac40d5af679aa8eedde373&q=${typedInput}`)
+          .then(response => {
+            console.log(response, 'Response')
+            setData(response.data);
+          })
+      }}
+      >Search</button>
+        {data && (
+          <>
+          <div>
+            <p>{data.articles[0].title}</p>
+            <p>{data.articles[0].author}</p>
+            <img src={data.articles[0].urlToImage} />
+            <p>{data.articles[0].description}</p>
+            <p>{data.articles[0].url}</p>
+          </div>
+          </>
+        )}
     </div>
   );
 }
