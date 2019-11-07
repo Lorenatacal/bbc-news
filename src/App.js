7cae430626a5a37d0066426fe2f01dcd9dc30c43
Lorena <lorena.tacal@gmail.com>
2019-11-07 00:40:43 +0000

    Refactor App component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import axios from 'axios';
 import './App.css';
 import Header from './components/Header';
-import MainPage from './components/MainPage'
+import MainPage from './components/MainPage';
+import Weather from './components/Weather'
 
 function App() {
 
   return (
     <div className="App">
       <Header />
+      <Weather />
       <MainPage />
     </div>
   );
