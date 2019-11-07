6d392e7e92a0691d232d98d2e91cdc5a392c4b54
Lorena <lorena.tacal@gmail.com>
2019-11-07 14:31:55 +0000

    Create API for the Book component

---
+++ src/components/Books.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import axios from 'axios';
+
+
+function Books() {
+  const [data, setData] = React.useState();
+
+  React.useEffect(() => {
+    axios
+    .get(`https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=2VwRbeVmwKCp6P2xCiOqG1wGWkTdYeq2`)
+    .then(response => {
+      setData(response.data)
+    });
+  }, [])
+
+  return (
+    <div className="App">
+      <h1>Books</h1>
+      <div className='cards'>
+        {data && data.results.books.map(({ title, author, description, book_image }) => {
+          return (
+              <div className='card'>
+                <p>{title}</p>
+                <img className="image" src={book_image} />
+                <p>{author}</p>
+                <p>{description}</p>
+              </div>
+          )
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Books;
